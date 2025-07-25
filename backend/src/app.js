ee12ae8f969b0858d5d79f4d9cef1112daddae2a
masterShashi <shashikumargupta443@gmail.com>
2025-07-25 21:50:06 +0530

    add cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,12 +6,30 @@ const configureCloudinary = require('./config/cloudinary');
 const mainRouter = require('./routes/index');
 const cookieParser = require('cookie-parser');
 const healthRouter = require('./routes/health.routes');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
 configureCloudinary();
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
