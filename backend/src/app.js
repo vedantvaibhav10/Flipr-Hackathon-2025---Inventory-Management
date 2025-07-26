c73c92ebddfd0b1c3da9396892409b7c167fa32a
masterShashi <shashikumargupta443@gmail.com>
2025-07-26 13:54:34 +0530

    Update CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -22,14 +22,17 @@ const allowedOrigins = [
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin) return callback(null, true);
+
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
     return callback(null, true);
   },
-  credentials: true
+  credentials: true,
 }));
+
+app.options('*', cors());
 
 app.use(express.json());
 app.use(cookieParser());
